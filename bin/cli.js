8ca6bc2a13b5aae95e5dce878b6355edebe73f48
汤启民 <tangiming@foxmail.com>
2021-08-26 15:34:51 +0800

    feat: 基本完成

--- bin/cli.js
+++ bin/cli.js
@@ -1,25 +1,55 @@
 #! /usr/bin/env node
 
-// #! 符号的名称叫 Shebang，用于指定脚本的解释程序
-// Node CLI 应用入口文件必须要有这样的文件头
-// 如果是Linux 或者 macOS 系统下还需要修改此文件的读写权限为 755
-// 具体就是通过 chmod 755 cli.js 实现修改
-import commander, { program } from 'commander'
-import { createRequire } from 'module'
-import create from '../lib/create.js'
-const require = createRequire(import.meta.url)
+const program = require('commander');
+const figlet = require('figlet');
+const chalk = require('chalk');
+
+
 program
   .command('create <app-name>')
   .description('create a new project')
-  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
-  .option('-f, --force', 'overwrite target directory if it exist')
+  .option('-f, --force', 'overwrite target directory if it exist') // 是否强制创建，当文件夹已经存在
   .action((name, options) => {
-    create(name, options)
+    // 在 create.js 中执行创建任务
+    require('../lib/create.js')(name, options)
+  })
+
+// 配置 config 命令
+program
+  .command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <path>', 'get value from option')
+  .option('-s, --set <path> <value>')
+  .option('-d, --delete <path>', 'delete option from config')
+  .action((value, options) => {
+    console.log(value, options)
+  })
+
+// 配置 ui 命令
+program
+  .command('ui')
+  .description('start add open roc-cli ui')
+  .option('-p, --port <port>', 'Port used for the UI Server')
+  .action((option) => {
+    console.log(option)
   })
 
 program
+  // 配置版本号信息
   .version(`v${require('../package.json').version}`)
   .usage('<command> [option]')
 
-//解析用户执行命令的时候输入的参数
-program.parse(process.argv)
+program
+  .on('--help', () => {
+    console.log('\r\n' + figlet.textSync('zhurong', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    }));
+    console.log(`\r\nRun ${chalk.cyan(`zr <command> --help`)} for detailed usage of given command\r\n`)
+  })
+
+// 解析用户执行命令传入参数
+program.parse(process.argv);
